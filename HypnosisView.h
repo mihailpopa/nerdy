0818795b44303420f3c2caf26f9e10440d53ef4b
Mihai Popa <mihaipopa@Mihais-Mac-Pro.local>
2012-07-10 01:32:37 +0300

    Hypnosis finished day1

---
+++ HypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  HypnosisView.h
+//  Hypnosister
+//
+//  Created by Mihai Popa on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisView : UIView
+
+@end
---
+++ HypnosisView.m
@@ -0,0 +1,31 @@
+//
+//  HypnosisView.m
+//  Hypnosister
+//
+//  Created by Mihai Popa on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HypnosisView.h"
+
+@implementation HypnosisView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
